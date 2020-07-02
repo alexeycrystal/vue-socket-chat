f66ffcfd48ba878c94d47ca610e16e64efc14369
shabramov <alex.shabramov@rankactive.com>
2020-07-02 17:46:48 +0300

    Addition - Messages - message search & lazy scrolling & message view by search;

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <div>
     <router-view></router-view>
-    <button @click="computeData">LOLKA</button>
   </div>
 </template>
 
 <script>
   export default {
-    methods: {
-      computeData() {
-        let test = new URLSearchParams('http://localhost:8000/api/user/messages?per_page=20&page=1');
-        console.log(test.get('page'));
-      }
-    }
   }
 </script>
 
